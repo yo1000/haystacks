5a3437b9469e4492aaabb471ea37637a4fd42f74
yo1000 <yoichi.kikuchi.aug@gmail.com>
2018-07-09 01:25:07 +0900

    Add implementation to part of MysqlTableRepository

--- haystacks-assembly-mysql/src/main/kotlin/com/yo1000/haystacks/infrastructure/repository/mysql/MysqlTableRepository.kt
+++ haystacks-assembly-mysql/src/main/kotlin/com/yo1000/haystacks/infrastructure/repository/mysql/MysqlTableRepository.kt
@@ -1,12 +1,13 @@
 package com.yo1000.haystacks.infrastructure.repository.mysql
 
-import com.yo1000.haystacks.domain.entity.Column
-import com.yo1000.haystacks.domain.entity.FoundNamednessMap
-import com.yo1000.haystacks.domain.entity.Index
-import com.yo1000.haystacks.domain.entity.TableNamedness
+import com.yo1000.haystacks.domain.entity.FoundNamesMap
+import com.yo1000.haystacks.domain.entity.Table
+import com.yo1000.haystacks.domain.entity.TableNames
 import com.yo1000.haystacks.domain.repository.TableRepository
-import com.yo1000.haystacks.domain.valueobject.Statement
+import com.yo1000.haystacks.domain.valueobject.LogicalName
 import com.yo1000.haystacks.domain.valueobject.TablePhysicalName
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Repository
 
 /**
@@ -14,24 +15,34 @@ import org.springframework.stereotype.Repository
  * @author yo1000
  */
 @Repository
-class MysqlTableRepository : TableRepository {
-    override fun findNamedness(vararg q: String): FoundNamednessMap {
+class MysqlTableRepository(
+        private val dataSourceProperties: DataSourceProperties,
+        private val jdbcTemplate: NamedParameterJdbcTemplate
+) : TableRepository {
+    override fun findNames(vararg q: String): FoundNamesMap {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun findTableNamedness(name: TablePhysicalName): TableNamedness {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun findTableNamesAll(): List<TableNames> = jdbcTemplate.query("""
+        SELECT
+            tbl.table_name    AS `name`,
+            tbl.table_comment AS `comment`
+        FROM
+            information_schema.tables tbl
+        WHERE
+            tbl.table_schema = :schemaName
+        AND
+            tbl.table_type = 'BASE TABLE'
+        """.trimIndent(), mapOf(
+            "schemaName" to dataSourceProperties.name
+    )) { resultSet, _ ->
+        TableNames(
+                physicalName = TablePhysicalName(resultSet.getString("name")),
+                logicalName = LogicalName(resultSet.getString("comment"))
+        )
     }
 
-    override fun findColumns(name: TablePhysicalName): List<Column> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun findIndexes(name: TablePhysicalName): List<Index> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun findStatement(name: TablePhysicalName): Statement {
+    override fun findTable(name: TablePhysicalName): Table {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
@@ -39,9 +50,21 @@ class MysqlTableRepository : TableRepository {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun findRowCountMap(): Map<TablePhysicalName, Long> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun findRowCountMap(): Map<TablePhysicalName, Long>  = jdbcTemplate.query("""
+        SELECT
+            tbl.table_name    AS `name`,
+            tbl.table_rows    AS `rowSize`
+        FROM
+            information_schema.tables tbl
+        WHERE
+            tbl.table_schema = :schemaName
+        AND
+            tbl.table_type = 'BASE TABLE'
+        """.trimIndent(), mapOf(
+            "schemaName" to dataSourceProperties.name
+    )) { resultSet, _ ->
+        TablePhysicalName(resultSet.getString("name")) to resultSet.getLong("rowSize")
+    }.toMap()
 
     override fun findReferencedCountFromChildrenMap(): Map<TablePhysicalName, Int> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
@@ -50,4 +73,5 @@ class MysqlTableRepository : TableRepository {
     override fun findReferencingCountToParentMap(): Map<TablePhysicalName, Int> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
 }
