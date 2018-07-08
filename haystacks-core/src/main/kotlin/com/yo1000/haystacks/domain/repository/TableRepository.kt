b4a5f3d427de8802b6ff54016b3b2a4bb5e33751
yo1000 <yoichi.kikuchi.aug@gmail.com>
2018-07-08 13:00:33 +0900

    Update implementations due to changed models

--- haystacks-core/src/main/kotlin/com/yo1000/haystacks/domain/repository/TableRepository.kt
+++ haystacks-core/src/main/kotlin/com/yo1000/haystacks/domain/repository/TableRepository.kt
@@ -1,21 +1,23 @@
 package com.yo1000.haystacks.domain.repository
 
 import com.yo1000.haystacks.domain.entity.Column
+import com.yo1000.haystacks.domain.entity.FoundNamednessMap
 import com.yo1000.haystacks.domain.entity.Index
-import com.yo1000.haystacks.domain.entity.Object
-import com.yo1000.haystacks.domain.entity.Table
-import com.yo1000.haystacks.domain.valueobject.TableName
+import com.yo1000.haystacks.domain.entity.TableNamedness
+import com.yo1000.haystacks.domain.valueobject.Statement
+import com.yo1000.haystacks.domain.valueobject.TablePhysicalName
 
 /**
  * @author yo1000
  */
 interface TableRepository {
-    fun findTableObjects(vararg q: String): List<Object>
-    fun findTable(name: TableName): Table
-    fun findColumns(name: TableName): List<Column>
-    fun findIndexes(name: TableName): List<Index>
-    fun findColumnCountMap(): Map<TableName, Int>
-    fun findRowCountMap(): Map<TableName, Long>
-    fun findReferencedCountFromChildrenMap(): Map<TableName, Int>
-    fun findReferencingCountToParentMap(): Map<TableName, Int>
+    fun findNamedness(vararg q: String): FoundNamednessMap
+    fun findTableNamedness(name: TablePhysicalName): TableNamedness
+    fun findColumns(name: TablePhysicalName): List<Column>
+    fun findIndexes(name: TablePhysicalName): List<Index>
+    fun findStatement(name: TablePhysicalName): Statement
+    fun findColumnCountMap(): Map<TablePhysicalName, Int>
+    fun findRowCountMap(): Map<TablePhysicalName, Long>
+    fun findReferencedCountFromChildrenMap(): Map<TablePhysicalName, Int>
+    fun findReferencingCountToParentMap(): Map<TablePhysicalName, Int>
 }
--- haystacks-core/src/main/kotlin/com/yo1000/haystacks/domain/service/TableService.kt
+++ haystacks-core/src/main/kotlin/com/yo1000/haystacks/domain/service/TableService.kt
@@ -1,9 +1,10 @@
 package com.yo1000.haystacks.domain.service
 
 import com.yo1000.haystacks.domain.entity.Table
+import com.yo1000.haystacks.domain.entity.TableNamedness
 import com.yo1000.haystacks.domain.entity.TableOutline
 import com.yo1000.haystacks.domain.repository.TableRepository
-import com.yo1000.haystacks.domain.valueobject.TableName
+import com.yo1000.haystacks.domain.valueobject.TablePhysicalName
 import org.springframework.stereotype.Service
 
 /**
@@ -14,8 +15,14 @@ import org.springframework.stereotype.Service
 class TableService(
         private val tableRepository: TableRepository
 ) {
-    fun getTable(name: TableName): Table {
-        TODO("thinking..")
+    fun getTable(name: TablePhysicalName): Table = tableRepository.findTableNamedness(name).let {
+        Table(
+                namedness = it,
+                columns = tableRepository.findColumns(name),
+                indexes = tableRepository.findIndexes(name),
+                rowCount = tableRepository.findRowCountMap()[it.physicalName] ?: 0L,
+                statement = tableRepository.findStatement(name)
+        )
     }
 
     fun getTablesAll(): List<TableOutline> {
@@ -24,14 +31,16 @@ class TableService(
         val childrenCountMap = tableRepository.findReferencedCountFromChildrenMap()
         val parentCountMap = tableRepository.findReferencingCountToParentMap()
 
-        return tableRepository.findTableObjects().map {
+        return tableRepository.findNamedness().map {
             TableOutline(
-                    name = TableName(it.name.value),
-                    comment = it.comment,
-                    columnCount = columnCountMap[it.name] ?: 0,
-                    rowCount = rowCountMap[it.name] ?: 0L,
-                    referencedCountFromChildren = childrenCountMap[it.name] ?: 0,
-                    referencingCountToParent = parentCountMap[it.name] ?: 0
+                    namedness = TableNamedness(
+                            tablePhysicalName = TablePhysicalName(it.key.physicalName.value),
+                            logicalName = it.key.logicalName
+                    ),
+                    columnCount = columnCountMap[it.key.physicalName] ?: 0,
+                    rowCount = rowCountMap[it.key.physicalName] ?: 0L,
+                    referencedCountFromChildren = childrenCountMap[it.key.physicalName] ?: 0,
+                    referencingCountToParent = parentCountMap[it.key.physicalName] ?: 0
             )
         }
     }
