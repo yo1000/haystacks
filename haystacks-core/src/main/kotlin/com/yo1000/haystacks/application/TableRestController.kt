c1fecb58462405dae9af882ffe27253d46e6799a
yo1000 <yoichi.kikuchi.aug@gmail.com>
2018-07-16 14:33:33 +0900

    Add table controller

--- haystacks-core/src/main/kotlin/com/yo1000/haystacks/application/TableRestController.kt
+++ haystacks-core/src/main/kotlin/com/yo1000/haystacks/application/TableRestController.kt
@@ -1,8 +1,20 @@
 package com.yo1000.haystacks.application
 
+import com.yo1000.haystacks.domain.entity.TableOutline
+import com.yo1000.haystacks.domain.service.TableService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
 /**
  *
  * @author yo1000
  */
-class TableRestController {
+@RestController
+@RequestMapping("/tables")
+class TableRestController(
+        private val tableService: TableService
+) {
+    @GetMapping("")
+    fun get(): List<TableOutline> = tableService.getTableOutlines()
 }
