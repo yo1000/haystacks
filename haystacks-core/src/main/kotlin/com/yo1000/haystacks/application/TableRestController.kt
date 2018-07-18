967d2056a520a5fc7c676d1dadea885f5ea60879
yo1000 <yoichi.kikuchi.aug@gmail.com>
2018-07-19 02:48:19 +0900

    Add get table endpoint

--- haystacks-core/src/main/kotlin/com/yo1000/haystacks/application/TableRestController.kt
+++ haystacks-core/src/main/kotlin/com/yo1000/haystacks/application/TableRestController.kt
@@ -1,8 +1,11 @@
 package com.yo1000.haystacks.application
 
+import com.yo1000.haystacks.domain.entity.Table
 import com.yo1000.haystacks.domain.entity.TableOutline
 import com.yo1000.haystacks.domain.service.TableService
+import com.yo1000.haystacks.domain.valueobject.TablePhysicalName
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -17,4 +20,7 @@ class TableRestController(
 ) {
     @GetMapping("")
     fun get(): List<TableOutline> = tableService.getTableOutlines()
+
+    @GetMapping("/{name}")
+    fun getName(@PathVariable name: String): Table = tableService.getTable(TablePhysicalName(name))
 }
