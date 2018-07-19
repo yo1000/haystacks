2ab1216549c7b602be5822458d6b5e086ad80b8d
yo1000 <yoichi.kikuchi.aug@gmail.com>
2018-07-20 01:11:38 +0900

    Add index finding interface

--- haystacks-core/src/main/kotlin/com/yo1000/haystacks/domain/repository/IndexRepository.kt
+++ haystacks-core/src/main/kotlin/com/yo1000/haystacks/domain/repository/IndexRepository.kt
@@ -1,19 +1,11 @@
 package com.yo1000.haystacks.domain.repository
 
-import com.yo1000.haystacks.domain.entity.FoundNamesMap
-import com.yo1000.haystacks.domain.entity.Table
-import com.yo1000.haystacks.domain.entity.TableNames
+import com.yo1000.haystacks.domain.entity.Index
 import com.yo1000.haystacks.domain.valueobject.TablePhysicalName
 
 /**
  * @author yo1000
  */
-interface TableRepository {
-    fun findNames(vararg q: String): FoundNamesMap
-    fun findTableNamesAll(): List<TableNames>
-    fun findTable(name: TablePhysicalName): Table
-    fun findColumnCountMap(): Map<TablePhysicalName, Int>
-    fun findRowCountMap(): Map<TablePhysicalName, Long>
-    fun findReferencedCountFromChildrenMap(): Map<TablePhysicalName, Int>
-    fun findReferencingCountToParentMap(): Map<TablePhysicalName, Int>
+interface IndexRepository {
+    fun findByTableName(name: TablePhysicalName): List<Index>
 }
