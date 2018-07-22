07a4e012d96ffab8d3766777d26292e0502ec162
yo1000 <yoichi.kikuchi.aug@gmail.com>
2018-07-22 11:15:49 +0900

    Optimize import

--- haystacks-autoconfigure/src/main/kotlin/com/yo1000/haystacks/autoconfigure/data/mysql/MysqlRepositoryAutoConfiguration.kt
+++ haystacks-autoconfigure/src/main/kotlin/com/yo1000/haystacks/autoconfigure/data/mysql/MysqlRepositoryAutoConfiguration.kt
@@ -5,14 +5,10 @@ import com.yo1000.haystacks.core.repository.TableRepository
 import com.yo1000.haystacks.data.repository.mysql.MysqlIndexRepository
 import com.yo1000.haystacks.data.repository.mysql.MysqlTableRepository
 import org.springframework.boot.autoconfigure.AutoConfigureAfter
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
-import org.springframework.boot.autoconfigure.jdbc.JdbcProperties
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.jdbc.core.JdbcTemplate
