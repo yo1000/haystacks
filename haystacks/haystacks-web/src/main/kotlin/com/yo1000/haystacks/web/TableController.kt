4303bb1cd0bafc7b5f81acc6a7ee7c0252106215
yo1000 <yoichi.kikuchi.aug@gmail.com>
2018-07-24 01:55:32 +0900

    Add table page

--- haystacks/haystacks-web/src/main/kotlin/com/yo1000/haystacks/web/TableController.kt
+++ haystacks/haystacks-web/src/main/kotlin/com/yo1000/haystacks/web/TableController.kt
@@ -2,6 +2,7 @@ package com.yo1000.haystacks.web
 
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 
 /**
@@ -15,5 +16,13 @@ open class TableController(
     fun get(model: Model): String {
         model.addAttribute("dataSourceName", dataSourceName)
         return "tables"
+    }
+
+    @GetMapping("/{tableName}")
+    fun getByName(model: Model, @PathVariable tableName: String): String {
+        model.addAttribute("dataSourceName", dataSourceName)
+        model.addAttribute("tableName", tableName)
+
+        return "table"
     }
 }
