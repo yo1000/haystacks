d26f3146bd622e8c3963a9cc4fc4e13caeb78d28
yo1000 <yoichi.kikuchi.aug@gmail.com>
2018-10-18 03:15:28 +0900

    Apply additional dialect

--- haystacks-autoconfigure/src/main/kotlin/com/yo1000/haystacks/autoconfigure/web/WebAutoConfiguration.kt
+++ haystacks-autoconfigure/src/main/kotlin/com/yo1000/haystacks/autoconfigure/web/WebAutoConfiguration.kt
@@ -2,18 +2,25 @@ package com.yo1000.haystacks.autoconfigure.web
 
 import com.yo1000.haystacks.web.controller.*
 import com.yo1000.haystacks.web.service.TableApplicationService
+import com.yo1000.haystacks.web.view.TablesDialect
+import org.springframework.beans.factory.ObjectProvider
 import org.springframework.boot.autoconfigure.AutoConfigureAfter
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.DispatcherServlet
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.thymeleaf.dialect.IDialect
+import org.thymeleaf.spring5.SpringTemplateEngine
+import org.thymeleaf.templateresolver.ITemplateResolver
 import javax.servlet.Servlet
 
 @Configuration
@@ -22,6 +29,18 @@ import javax.servlet.Servlet
 @AutoConfigureAfter(ApplicationServiceAutoConfiguration::class)
 @EnableConfigurationProperties(WebConfigurationProperties::class)
 class WebAutoConfiguration {
+    @Bean
+    fun springTemplateEngine(
+            props: ThymeleafProperties,
+            templateResolvers: Collection<ITemplateResolver>,
+            dialectsProvider: ObjectProvider<Collection<IDialect>>
+    ): SpringTemplateEngine = SpringTemplateEngine().also {
+        it.enableSpringELCompiler = props.isEnableSpringElCompiler
+        templateResolvers.forEach(it::addTemplateResolver)
+        dialectsProvider.getIfAvailable { emptyList() }.forEach(it::addDialect)
+        it.addDialect(TablesDialect())
+    }
+
     @RestController
     @ConditionalOnWebApplication(type = Type.SERVLET)
     @ConditionalOnClass(Servlet::class, DispatcherServlet::class, WebMvcConfigurer::class)
