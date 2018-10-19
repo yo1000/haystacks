e29109c348e49cb6a546dafe1279115b95f9bf33
yo1000 <yoichi.kikuchi.aug@gmail.com>
2018-10-20 01:42:53 +0900

    Add another version info-view for support to server side rendering

--- haystacks/haystacks-web/src/main/kotlin/com/yo1000/haystacks/web/controller/InformationController.kt
+++ haystacks/haystacks-web/src/main/kotlin/com/yo1000/haystacks/web/controller/InformationController.kt
@@ -1,6 +1,9 @@
 package com.yo1000.haystacks.web.controller
 
+import com.yo1000.haystacks.web.resource.DataSource
+import com.yo1000.haystacks.web.service.TableApplicationService
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
@@ -13,7 +16,15 @@ open class InformationController(
         private val dataSourceProperties: DataSourceProperties
 ) {
     @GetMapping("")
-    fun get(): String {
-        return "info"
+    fun get(model: Model): String = if (ssr) {
+        model.addAttribute("dataSource", DataSource(
+                url = dataSourceProperties.url,
+                name = dataSourceProperties.name,
+                username = dataSourceProperties.username,
+                driverClass = dataSourceProperties.driverClassName
+        ))
+        "info.ssr"
+    } else {
+        "info"
     }
 }
